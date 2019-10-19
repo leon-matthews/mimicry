34c9bcfefbe1c165d34f5ba42c3fec42e6ba1aba
Leon Matthews <leon@digitaladvisor.nz>
2019-10-19 13:14:09 +1300

    Make coverage.sh script cross-platform with Mac OS X + MacPorts

--- coverage.sh
+++ coverage.sh
@@ -3,6 +3,9 @@
 # Run tests on application.
 # Show coverage report if all tests pass.
 
+# From the excellent Ned Batchelder
+# https://coverage.readthedocs.io/
+
 set -o nounset
 set -o errexit
 set +o xtrace
@@ -13,8 +16,19 @@ if [ $# -ne 0 ]; then
     exit 1
 fi
 
-COVERAGE=coverage-3.7
 
+# Find coverage command in Debian/Ubuntu or Mac OS + MacPorts
+if [ -x "$(command -v python3-coverage)" ]; then
+    COVERAGE=python3-coverage
+elif [ -x "$(command -v coverage-3.7)" ]; then
+    COVERAGE=coverage-3.7
+else
+    echo "No Python coverage command found"
+    exit 1
+fi
+
+
+# Run unit tests under the supervision of 'coverage.py'
 $COVERAGE run --branch --module unittest --failfast --catch
 $COVERAGE report --show-missing
 rm -f .coverage
